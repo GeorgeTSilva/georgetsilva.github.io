1b8c7d1f5e530431e2e73c4e870e6105f2e18709
GeorgeTSilva <georges083@gmail.com>
2017-01-22 22:30:52 -0500

    Nikola auto commit.
    Nikola version: 7.8.3

--- conf.py
+++ conf.py
@@ -146,7 +146,7 @@ NAVIGATION_LINKS = {
 }
 
 # Name of the theme to use.
-THEME = "zen-ipython"
+THEME = "custom-zen-ipython"
 
 # Primary color of your theme. This will be used to customize your theme and
 # auto-generate related colors in POSTS_SECTION_COLORS. Must be a HEX value.
